cac741a63092581cbc56a71258f96f6a904790a2
LilliMi <lilli.mich@web.de>
2021-02-17 19:01:53 +0100

    Added check answer and environment
    Added check answer and environment

--- Assets/ButtonControll.cs
+++ Assets/ButtonControll.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,14 +12,30 @@ public class ButtonControll : MonoBehaviour
     private float buttonHitAgainTime = 0.5f;
     private float canHitAgain;
 
+    // Check Ball - Objects
+    private GameObject QuestionMark;
+    private GameObject Number31;
+
+    // TextMessages
+    private GameObject Congratz;
+    private GameObject WrongNumber;
+
     // Start is called before the first frame update
     void Start()
     {
-        //source = gameObject.AddComponent // für Sound später
+        //Button
+            //source = gameObject.AddComponent // für Sound später
 
-        button = transform.GetChild(0).gameObject;
-        buttonOriginalY = button.transform.position.y;
+            button = transform.GetChild(0).gameObject;
+            buttonOriginalY = button.transform.position.y;
 
+        //Balls
+        QuestionMark = GameObject.FindWithTag("QuestionMark");
+        Number31 = GameObject.FindWithTag("Number31");
+        Congratz = GameObject.FindWithTag("Congratz");
+        WrongNumber = GameObject.FindWithTag("Wrong");
+        Congratz.SetActive(false);
+        WrongNumber.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,19 +44,45 @@ public class ButtonControll : MonoBehaviour
          if(buttonHit == true){
              buttonHit = false;
              button.transform.position = new Vector3(button.transform.position.x, button.transform.position.y - buttonDownDistance, button.transform.position.z);
-
+             Debug.Log(QuestionMark.transform.position);
+             //Debug.Log(QuestionMark.getposition);
+             Debug.Log(Number31.transform.position);
          }
 
          if(button.transform.position.y < buttonOriginalY){
              button.transform.position += new Vector3(0, buttonReturnSpeed, 0);
 
          }
+        //System.out.print("Hallu");
+        //Console.WriteLine("Hallu");
     }
 
     private void OnTriggerEnter(Collider other) {
         if(other.CompareTag("PlayerHand") && canHitAgain < Time.time){
             canHitAgain = Time.time + buttonHitAgainTime;
             buttonHit = true;
+
+            Congratz.SetActive(false);
+            WrongNumber.SetActive(false);
+
+
+            if (QuestionMark != null && Number31 != null)
+            {
+                double distance = (Vector3.Distance(QuestionMark.transform.position, Number31.transform.position));
+                double sumRadius = ((QuestionMark.GetComponent<SphereCollider>().radius*QuestionMark.transform.localScale.x) + (Number31.GetComponent<SphereCollider>().radius*Number31.transform.localScale.x));
+
+                Debug.Log(string.Format("Distance between {0} and {1} is: {2}", QuestionMark, Number31, distance));
+                Debug.Log(string.Format("QuestionMark: {0}", (QuestionMark.GetComponent<SphereCollider>().radius*QuestionMark.transform.localScale.x)));
+                Debug.Log(string.Format("Number31: {0}", (Number31.GetComponent<SphereCollider>().radius*Number31.transform.localScale.x)));
+                Debug.Log(string.Format("sumRadius: {0}", sumRadius));
+
+                if(distance <= sumRadius){
+                    Congratz.SetActive(true);
+                } else{
+                    WrongNumber.SetActive(true);
+                }
+
+            }
         }
 
     }
